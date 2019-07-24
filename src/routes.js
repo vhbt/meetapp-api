bfc3e29741cf89de52bcfdd13607e3934448c770
Vitor Hariel <vhbarauna@gmail.com>
2019-07-24 01:42:31 -0300

    testing deploy

--- src/routes.js
+++ src/routes.js
@@ -24,8 +24,8 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.put('/users', UserController.update);
 
 routes.get('/meetups', MeetupController.index);
-routes.post('/meetups', MeetupController.store);
 routes.get('/meetups/:id', MeetupController.view);
+routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
 
