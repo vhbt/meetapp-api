40d1b2412bbd1482844d655766c85c367bcb6c61
Vitor Hariel <vhbarauna@gmail.com>
2019-07-25 11:34:03 -0300

    Fix deploy banner image

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,7 +9,7 @@ class File extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `http://localhost:3333/files/${this.path}`;
+            return `https://api.meetapp.ninjastic.space/files/${this.path}`;
           },
         },
       },
