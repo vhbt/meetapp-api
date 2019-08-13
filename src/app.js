e08c4a7e41dd1cccee3a48d740b6ad28f5f63f46
Vitor Hariel <vhbarauna@gmail.com>
2019-08-13 17:37:51 -0300

    Undo: disable error handler to debug production

--- src/app.js
+++ src/app.js
@@ -38,16 +38,16 @@ class App {
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      // if (
-      //   process.env.NODE_ENV === 'development' ||
-      //   process.env.NODE_ENV === 'test'
-      // ) {
-      const errors = await new Youch(err, req).toJSON();
+      if (
+        process.env.NODE_ENV === 'development' ||
+        process.env.NODE_ENV === 'test'
+      ) {
+        const errors = await new Youch(err, req).toJSON();
 
-      return res.status(500).json(errors);
-      // }
+        return res.status(500).json(errors);
+      }
 
-      // return res.status(500).json({ error: 'Internal server error.' });
+      return res.status(500).json({ error: 'Internal server error.' });
     });
   }
 }
