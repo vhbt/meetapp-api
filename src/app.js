a451fb44df86ff0f81537d6f70b12dd5061a0fba
Vitor Hariel <vhbarauna@gmail.com>
2019-08-15 13:16:47 -0300

    add: brute force, rate limit and helmet protection

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import express from 'express';
 import Youch from 'youch';
 import { resolve } from 'path';
 import cors from 'cors';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import 'express-async-errors';
 import routes from './routes';
 import SentryConfig from './config/sentry';
@@ -24,11 +28,26 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
+    this.server.use(helmet());
     this.server.use(express.json());
     this.server.use(
       '/files',
       express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 200,
+        })
+      );
+    }
   }
 
   routes() {
